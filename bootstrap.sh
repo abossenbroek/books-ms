262bb786443ee8a7a1cd4915085a744e44caa527
Anton Bossenbroek <anton.bossenbroek@me.com>
2016-08-22 22:01:57 -0400

    Added to bootstrap of Vagrant dev machine that docker GPG public key should be added.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,6 @@ apt-get install -y software-properties-common
 apt-add-repository ppa:ansible/ansible
 apt-get update
 apt-get install -y --force-yes ansible
+
+echo "Adding Docker public key to dpkg list..."
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
